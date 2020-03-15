d4bdcc5888983d2c003f7f7ae5e16d004d89d8f0
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-03-15 17:37:47 -0500

    Pushing initial code with correct database connections.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Happy Meal Burger');
+INSERT INTO burgers (name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (name, devour) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devour) VALUES ('Double Bacon Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Chicken Sandwich', true);
+INSERT INTO burgers (name) VALUES ('Whoopper');
