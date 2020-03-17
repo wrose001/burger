b0169b693ab64f46fb85b210e35eb0064d44e40b
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-03-16 21:03:11 -0500

    Made name and value changes on controllers, models, and public folders.

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,4 @@ INSERT INTO burgers (name) VALUES ('Quarter Pounder');
 INSERT INTO burgers (name, devour) VALUES ('Big Mac', true);
 INSERT INTO burgers (name, devour) VALUES ('Double Bacon Burger', true);
 INSERT INTO burgers (name, devour) VALUES ('Chicken Sandwich', true);
-INSERT INTO burgers (name) VALUES ('Whoopper');
+INSERT INTO burgers (name) VALUES ('Whooper');
